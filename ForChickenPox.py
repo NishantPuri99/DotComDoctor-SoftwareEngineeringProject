3b28fe06e2055288a1b16a8d384b9b2ecffc16d5
NishantPuri99 <nishant99puri@gmail.com>
2020-11-14 00:54:46 +0530

    Perfectly Working Random Forest Classifer, Taking care of 5 diseases only

---
+++ ForChickenPox.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import pickle
+
+df_CC = pd.read_csv('training_data.csv')
+df_CC.drop("Unnamed: 133",inplace=True,axis=1)
+df_CC.loc[df_CC['prognosis']!="Chicken pox","prognosis"] = "Not Chicken pox"
+X_train_CC = df_CC.drop('prognosis',axis=1).copy()
+y_train_CC = df_CC['prognosis'].copy()
+
+df_test_CC = pd.read_csv('test_data.csv')
+df_test_CC.loc[df_test_CC['prognosis']!="Chicken pox","prognosis"] = "Not Chicken pox"
+X_test_CC = df_test_CC.drop('prognosis',axis=1).copy()
+y_test_CC = df_test_CC['prognosis']
+
+clt_rf_CC = RandomForestClassifier()
+clt_rf_CC.fit(X_train_CC,y_train_CC)
+y_pred_CC = clt_rf_CC.predict(X_test_CC)
+#print(accuracy_score( y_test_CC, y_pred_CC))
+pickle.dump(clt_rf_CC,open("ChickenPox_pred.pkl","wb"))
+
+
--- server.py
+++ server.py
@@ -6,23 +6,20 @@ import json
 df = pd.read_csv('training_data.csv')
 input_symptoms = {}
 output_prediction = ""
-i_s = {}
-for i in df.columns:    
-    i_s[i] = random.randrange(0,2,1)
-# print(list(df["prognosis"]))
-prognosisList = []
-for i in list(df["prognosis"]):
-    if i not in prognosisList:
-        prognosisList.append(i)
+
+df.drop(df[["Unnamed: 133"]],axis=1,inplace=True)
+symptomsList = [x for x in df if x!='prognosis']
+
 app = Flask(__name__)
 app.secret_key = "abc"
-DTCModel = pickle.load(open('DTCmodel.pkl','rb'))
-RFCModel = pickle.load(open('RFCmodel.pkl','rb'))
-t_x = pd.read_json(json.dumps([i_s]))
-t_x.drop(columns=['prognosis','Unnamed: 133'],axis=1,inplace = True)
+CheckForJaundice = pickle.load(open('Jaundice_pred.pkl','rb'))
+CheckForDengue = pickle.load(open('Dengue_pred.pkl','rb'))
+CheckForCC = pickle.load(open('CC_pred.pkl','rb'))
+CheckForTB = pickle.load(open('TB_pred.pkl','rb'))
+CheckForCPox = pickle.load(open('ChickenPox_pred.pkl','rb'))
+
 @app.route('/',methods=['GET','POST'])
 def symptomGiver():
-    symptomsList = list(t_x.columns)
     if(request.method == 'POST'):
         chosen_symptoms = request.form.getlist('symptoms')
         for i in df.columns:
@@ -31,28 +28,53 @@ def symptomGiver():
             else:
                 input_symptoms[i] = 0
         test_x = pd.read_json(json.dumps([input_symptoms]))
-        test_x.drop(columns=['prognosis','Unnamed: 133'],axis=1,inplace = True)
-        DTCprediction = DTCModel.predict(test_x)
-        RFCprediction = RFCModel.predict(test_x)
-        DTCprobs = DTCModel.predict_proba(test_x)
-        RFCprobs = RFCModel.predict_proba(test_x)
-        print(DTCModel.predict_proba(test_x))
-        print(RFCModel.predict_proba(test_x))
-        DTCoutput = DTCprediction
-        RFCoutput = RFCprediction
-        DTCRes = {}
-        RFCRes = {}
-        for i in range(0,41):
-            if DTCprobs[0][i] != 0.0:
-                DTCRes[prognosisList[i]] = DTCprobs[0][i]*100
-        for i in range(0,41):
-            if RFCprobs[0][i] != 0.0:
-                RFCRes[prognosisList[i]] = RFCprobs[0][i]*100
-        #print(DTCRes)
-        #print(RFCRes)
+        # print(test_x.head())
+        # print(test_x.columns)
+        #test_x.drop(df[["Unnamed: 133"]],axis=1,inplace=True)
+        test_x.drop(df[["prognosis"]],axis=1,inplace=True)
+        #test_x.drop(columns=['prognosis','Unnamed: 133'],axis=1,inplace = True)
+        forTB = {'Prediction':CheckForTB.predict(test_x)[0],'Probs':{'HasDisease':CheckForTB.predict_proba(test_x)[0][1],'NotDiseased':CheckForTB.predict_proba(test_x)[0][0]}}
+        forCC = {'Prediction':CheckForCC.predict(test_x)[0],'Probs':{'HasDisease':CheckForCC.predict_proba(test_x)[0][0],'NotDiseased':CheckForCC.predict_proba(test_x)[0][1]}}
+        forJaundice = {'Prediction':CheckForJaundice.predict(test_x)[0],'Probs':{'HasDisease':CheckForJaundice.predict_proba(test_x)[0][0],'NotDiseased':CheckForJaundice.predict_proba(test_x)[0][1]}}
+        forDengue = {'Prediction':CheckForDengue.predict(test_x)[0],'Probs':{'HasDisease':CheckForDengue.predict_proba(test_x)[0][0],'NotDiseased':CheckForDengue.predict_proba(test_x)[0][1]}}
+        forCPox = {'Prediction':CheckForCPox.predict(test_x)[0],'Probs':{'HasDisease':CheckForCPox.predict_proba(test_x)[0][0],'NotDiseased':CheckForCPox.predict_proba(test_x)[0][1]}}
+        # print(CheckForCC.predict(test_x))
+        # print(CheckForJaundice.predict(test_x))
+        # print(CheckForDengue.predict(test_x))
+        # print(CheckForTB.predict(test_x))
+        # print(CheckForCPox.predict(test_x))
+        # print(CheckForCC.predict_proba(test_x))
+        # print(CheckForJaundice.predict_proba(test_x))
+        # print(CheckForDengue.predict_proba(test_x))
+        # print(CheckForTB.predict_proba(test_x))
+        # print(CheckForCPox.predict_proba(test_x))
+        # print(forTB)
+        # print(forCC)
+        # print(forJaundice)
+        # print(forDengue)
+        # print(forCPox)
+        Results = [forTB,forCC,forDengue,forJaundice,forCPox]
+        Predicted_Diagnosis = []
+        Predicted_Probability = []
+        ActualPredictionAvailable = False
+        for i in Results:
+            if i['Prediction'][:3] != 'Not':
+                Predicted_Diagnosis.append(i['Prediction'])
+                Predicted_Probability.append(i['Probs']['HasDisease']*100)
+                ActualPredictionAvailable = True
+        if not Predicted_Diagnosis:
+            for i in Results:
+                if(i['Probs']['HasDisease'] != 0.0):
+                    Predicted_Diagnosis.append(i['Prediction'][4:])
+                    Predicted_Probability.append(i['Probs']['HasDisease']*100)
         #output_prediction = {'RFC-result':df.iloc[RFCoutput[0],-2],'DTC-result':df.iloc[DTCoutput[0],-2],}
-        output_prediction = {'RFC-result':RFCRes,}
+        output_prediction = ""
+        if ActualPredictionAvailable:
+            output_prediction = (ActualPredictionAvailable,Predicted_Diagnosis[0],Predicted_Probability[0])
+        else:
+            output_prediction = (ActualPredictionAvailable,list(zip(Predicted_Diagnosis,Predicted_Probability)))
         session["diagnosis"] = output_prediction
+        #print(output_prediction)
         return redirect(url_for('diagnosis'))
     return render_template('index.html',variable = symptomsList)
 @app.route('/diagnosis')
